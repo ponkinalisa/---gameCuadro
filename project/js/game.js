4d983b711493fb2a6b989b635009022f7358f55d
ponkina <ponkinalisa77@gmail.com>
2025-03-28 21:21:07 +0700

    добавление анимации

--- project/js/game.js
+++ project/js/game.js
@@ -11,11 +11,19 @@ var batteryWidth = 30;
 const playerSpeed = 15;
 var win = false;
 var stop_all = false;
+var collision = false;
+var src = document.getElementById("game-container");
+let img = document.createElement("img");
+img.classList.add('boom');
+img.src = "project/img/boom.png";
+src.appendChild(img);
+var game = false;
 
 document.addEventListener("DOMContentLoaded", function () {
     document.getElementById('player').style.left = '10px';
 });
 
+document.getElementById('power').style.color = 'orange';
 
 function getRandomInt(min, max) {
     return Math.floor(Math.random() * (max - min)) + min;
@@ -41,6 +49,7 @@ function start(){
 }
 // Функция для старта игры
 function startGame(){
+    game = true;
     let player = document.getElementById('player-name').value;
     if (!player) {
         btn.style.filter = 'grayscale(1)';
@@ -49,9 +58,22 @@ function startGame(){
     btn.style.filter = 'grayscale(0)';
     localStorage.setItem('username', player);
     playerName = player;
+    let welcome = document.getElementById('welcome-screen');
+    let frame = 0;
+    const welcomeInterval = setInterval(() => {
+        if (frame === 100) {
+            clearInterval(welcomeInterval);
+            welcome.style.display = 'none';
+        } else {
+            if (stop_all == false){
+                welcome.style.left = `-${frame * 10}px`;
+                frame++;
+            }
+        }
+    }, 17); // Частота обновления ~60 FPS
 
     // Показываем игровое поле
-    document.getElementById('welcome-screen').style.display = 'none';
+
     document.getElementById('game-container').style.display = 'block';
 
     // Обновляем имя игрока
@@ -78,11 +100,18 @@ function updateTimer() {
     if (stop_all == false){
         powerLevel--;
     }
-    if (powerLevel <= 0){
+    if (powerLevel <= 0 && !win){
         endGame(true); // если игрок потерал весь заряд - экран результатов
     }
     document.getElementById('timer').innerText = `${minutes}:${seconds}`;
     document.getElementById('power').innerText = `Заряд: ${powerLevel}%`;
+    if (powerLevel < 20){
+        document.getElementById('power').style.color = 'red';
+    }else if (powerLevel < 60){
+        document.getElementById('power').style.color = 'orange';
+    }else{
+        document.getElementById('power').style.color = 'green';
+    }
 }
 
 // Генерация стен
@@ -161,6 +190,7 @@ function animateBattery(battery) {
 // Проверка столкновения с батареей
 function checkBatteryCollision(battery) {
     const playerRect = document.getElementById('player').getBoundingClientRect();
+    const player = document.getElementById('player');
     const batteryRect = battery.getBoundingClientRect();
 
     if (playerRect.left <= batteryRect.right &&
@@ -173,6 +203,25 @@ function checkBatteryCollision(battery) {
         }
         document.getElementById('power').innerText = `Заряд: ${powerLevel}%`;
         battery.remove(); // Удаляем аккумулятор
+        let frame = 0;
+        player.style.background = `green`;
+        const animInterval = setInterval(() => {
+            if (frame === 100) { // Убираем стену через определенное количество кадров
+                clearInterval(animInterval);
+                player.style.width = '60px';
+                player.style.height = '60px';
+                player.style.background = `red`;
+            } else {
+                if (stop_all == false){
+                    if (frame < 50){
+                        player.style.transform = `scale(${frame * 0.01 + 1})`;
+                    }else{
+                        player.style.transform = `scale(${2 - frame * 0.01})`;
+                    }
+                    frame++;
+                }
+            }
+        }, 17); // Частота обновления ~60 FPS
     }
 }
 
@@ -184,7 +233,28 @@ function checkCollision(wall) {
     if (playerRect.left <= wallRect.right &&
         playerRect.right >= wallRect.left &&
         playerRect.top <= wallRect.bottom &&
-        playerRect.bottom >= wallRect.top) {
+        playerRect.bottom >= wallRect.top) { 
+            console.log(collision);
+            if (collision == false){
+                collision = true;
+                let img1 = document.getElementsByClassName("boom")[0];
+                img1.style.display = 'block';
+                img1.style.top = `${playerRect.top - 20}px`;
+                let frame = 0;
+                const imgInterval = setInterval(() => {
+                    if (frame === 100) {
+                        clearInterval(imgInterval);
+                        img1.style.display = 'none';
+                    } else {
+                        if (stop_all == false){
+                            img.style.transform = `scale(${frame * 0.01 + 0.5})`;
+                            img.style.opacity = `${1 - frame * 0.01}`;
+                            frame++;
+                        }
+                    }
+                    }, 17); // Частота обновления ~60 FPS
+            }
+            //анимация столкновениe
             if (!win){
                 endGame(false); // Проигрыш
             }
@@ -235,9 +305,11 @@ function moveDown() {
 
 // Завершение игры
 function endGame(win) {
+    game = false;
     clearInterval(gameInterval);
     clearInterval(wallsInterval);
     clearInterval(batteriesInterval);
+    collision = false;
 
     if (win) {
         showResultScreen();
@@ -252,6 +324,18 @@ function showResultScreen() {
     document.getElementById('result-name').innerText = playerName;
     document.getElementById('result-time').innerText = document.getElementById('timer').innerText;
     document.getElementById('restart-button').onclick = restartGame;
+    let frame = 0;
+    let result = document.getElementById('result-screen');
+    const resultInterval = setInterval(() => {
+        if (frame === 100) {
+            clearInterval(resultInterval);
+        } else {
+            if (stop_all == false){
+                result.style.right = `${frame * 5 - 500}px`;
+                frame++;
+            }
+        }
+    }, 17); // Частота обновления ~60 FPS
 }
 
 // Показ экрана проигрыша
@@ -262,16 +346,55 @@ function showGameOverScreen() {
         document.getElementById('gameover-message').innerText = 'У вас закончился заряд!';
     }
     document.getElementById('gameover-restart').onclick = restartGame;
+    let frame = 0;
+    let result = document.getElementById('gameover-screen');
+    const resultInterval = setInterval(() => {
+        if (frame === 100) {
+            clearInterval(resultInterval);
+        } else {
+            if (stop_all == false){
+                result.style.right = `${frame * 5 - 500}px`;
+                frame++;
+            }
+        }
+    }, 17); // Частота обновления ~60 FPS
 }
 
 // Перезагрузка игры
 function restartGame() {
-    document.getElementById('result-screen').style.display = 'none';
-    document.getElementById('gameover-screen').style.display = 'none';
+    if (!game){
+        let frame = 0;
+    let result = document.getElementById('gameover-screen');
+    const resultInterval = setInterval(() => {
+        if (frame === 100) {
+            clearInterval(resultInterval);
+        } else {
+            if (stop_all == false){
+                result.style.right = `-${frame * 5}px`;
+                frame++;
+            }
+        }
+    }, 17); // Частота обновления ~60 FPS
+    let frame1 = 0;
+    let result1 = document.getElementById('result-screen');
+    const result1Interval = setInterval(() => {
+        if (frame1 === 100) {
+            clearInterval(result1Interval);
+            result1.style.right = '-500px';
+        } else {
+            if (stop_all == false){
+                result1.style.right = `-${frame1 * 5}px`;
+                frame1++;
+            }
+        }
+    }, 17); // Частота обновления ~60 FPS
     // Глобальные переменные
     timeElapsed = 0;
     powerLevel = 50;
+    stop_all = false;
     startGame();
+
+    }
 }
 
 
